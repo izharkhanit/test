1b0836d7b1b2fc1c4787aae3d9304dd962504716
izkhan <izkhan@LIN51005588.corp.capgemini.com>
2017-12-11 01:23:04 +0530

    Cloneable Interface

--- myTest.java
+++ myTest.java
@@ -1,7 +1,7 @@
-
+import InterfacesTypes.CloneableInterface;
 public class myTest {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws CloneNotSupportedException {
 		System.out.println("Angular + Java + API + Database =");
 		System.out.println("full stack developer");
 		System.out.println("asas sasas");
@@ -14,6 +14,19 @@ public class myTest {
 		System.out.println(s2.toString());
 		myTestClass t = new myTestClass();
         t.display();
+ 
+        //cloneable Interface example
+        CloneableInterface a = new CloneableInterface(27, "I am comming from Cloneable interface");
+
+    		// cloning 'a' and holding
+    		// new cloned object reference in b
+
+    		// down-casting as clone() return type is Object
+        CloneableInterface b = (CloneableInterface)a.clone();
+
+    		System.out.println(b.i);
+    		System.out.println(b.s);
+    	
         
 	}
 	
