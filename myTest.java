5142c0f4bd7f0e3e3668e63877559be3e64e15fa
izkhan <izkhan@LIN51005588.corp.capgemini.com>
2017-12-11 00:55:56 +0530

    Interfaces

--- myTest.java
+++ myTest.java
@@ -6,6 +6,15 @@ public class myTest {
 		System.out.println("full stack developer");
 		System.out.println("asas sasas");
 		System.out.println("asasas");
+
+		Shape s1 = new Circle("Red", 2.2);
+		Shape s2 = new Rectangle("Yellow", 2, 4);
+		
+		System.out.println(s1.toString());
+		System.out.println(s2.toString());
+		myTestClass t = new myTestClass();
+        t.display();
+        
 	}
 	
 }
