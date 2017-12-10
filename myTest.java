f07b32a398d514208b4766f2979cfd9c626aeff5
izkhan <izkhan@LIN51005588.corp.capgemini.com>
2017-12-10 23:42:23 +0530

    Test

---
+++ myTest.java
@@ -0,0 +1,11 @@
+
+public class myTest {
+
+	public static void main(String[] args) {
+		System.out.println("Angular + Java + API + Database =");
+		System.out.println("full stack developer");
+		System.out.println("asas sasas");
+		
+	}
+	
+}
